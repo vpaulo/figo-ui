b5593e4bd268cb485e44505fcc9eca384cb7e291
Vitor Rodrigues <vpaulo@users.noreply.github.com>
2025-07-02 19:54:17 +0100

    Added components preview

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,15 +14,20 @@ function init() {
 
   window.addEventListener("load-component", async (e) => {
     const content = document.querySelector("fg-content");
+    assert(!content, "fg-content element not found");
     const [_, code] = await tryCatch(await GetComponentById(e.detail));
-    assert(!code, "Get component CSS and HTML failed");
+    assert(!code && !Array.isArray(code), "Get component CSS and HTML failed");
+    assert(code.length !== 2, "Code returned is not valid");
 
     const [css, html] = code;
     content.cssCode = css.trim(); // TODO: run biome or prettier on code
     content.htmlCode = html.trim(); // TODO: run biome or prettier on code
+    content.preview = code;
 
     // Show css of component selected
-    content.querySelector("#tab2")?.click();
+    const tab = content.querySelector("#tab2");
+    assert(!tab, "tab element not found");
+    content.querySelector("#tab2").click();
   });
 }
 
@@ -33,7 +38,14 @@ async function getComponents() {
   const tokensStyle = document.getElementById("tokens");
   const list = document.querySelector("fg-list");
   const content = document.querySelector("fg-content");
+  assert(!page, "page input not found");
+  assert(!token, "token input not found");
+  assert(!prefix, "prefix input not found");
+  assert(!tokensStyle, "tokens style element not found");
+  assert(!list, "fg-list element not found");
+  assert(!content, "fg-content element not found");
 
+  // TODO: verify if errors returned from BE are verified
   const [e, components] = await tryCatch(
     await LoadComponentsFromApi({
       page: page?.value,
